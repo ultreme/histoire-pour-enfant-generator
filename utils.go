a3fb1d834b8f03369804272df2e7c887419da2bc
Manfred Touron <m@42.am>
2020-04-02 10:34:14 +0200

    feat: avoid panic

--- utils.go
+++ utils.go
@@ -3,6 +3,9 @@ package hpeg
 import "math/rand"
 
 func PickAndDelete(list *[]string) string {
+	if len(*list) < 1 {
+		return "ERROR"
+	}
 	lst := *list
 	idx := rand.Intn(len(lst))
 	picked := lst[idx]
